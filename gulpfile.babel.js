83c40be9859080ff0f6a3b73ce954ee8c8f3da59
Niko Kommenda <nkommenda@hotmail.com>
2017-09-07 17:57:08 +0100

    reverse

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -31,7 +31,7 @@ const browser = browserSync.create();
 const buildDir = '.build';
 const cdnUrl = 'https://interactive.guim.co.uk';
 
-const isDeploy = gutil.env._.indexOf('deploy') > -1 || gutil.env._.indexOf('deploylive') || gutil.env._.indexOf('deploypreview');
+const isDeploy = gutil.env._.indexOf('deploy') > -1 || gutil.env._.indexOf('deploylive') > -1 || gutil.env._.indexOf('deploypreview') > -1;
 
 const version = `v/${Date.now()}`;
 const s3Path = `atoms/${config.path}`;
--- src/server/convert.js
+++ src/server/convert.js
@@ -5,7 +5,7 @@ import * as turf from '@turf/turf'
 const cone = JSON.parse(fs.readFileSync('data/' + process.argv[2]))
 const dates = JSON.parse(fs.readFileSync('data/' + process.argv[3]))
 
-const categories = [["<5%", "5-10%"], ["40-50%", "30-40%", "20-30%", "10-20%"], ["50-60%", "60-70%", "70-80%", "80-90%"], [">90%"]]
+const categories = [["<5%", "5-10%"], ["40-50%", "30-40%", "20-30%", "10-20%"].reverse(), ["50-60%", "60-70%", "70-80%", "80-90%"], [">90%"]]
 const titles = ['<10%', '10-50%', '50-90%', '>90%']
 
 const key = Object.keys(cone.objects)[0]
