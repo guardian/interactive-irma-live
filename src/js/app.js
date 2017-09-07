34db1a69967f0add989a31e0a186ba8736c03b51
Josh Holder <josh.holder@theguardian.com>
2017-09-07 14:44:03 +0100

    More

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
 import GoogleMap from '../components/interactive-google-map/index.html'
 import path from '../assets/path.json'
 
+import history from '../assets/history-working.json'
+
+// import history from '../assets/history.json'
+
 const el = document.querySelector(".here .map")
 
 function getColour(p) {
@@ -15,11 +19,19 @@ function getColour(p) {
     }
 }
 
-const markers = path.features.map(d => {
+let markers = path.features.map(d => {
     return {
         "lat": d.properties.LAT,
         "lng": d.properties.LON,
         "label": d.properties.DATELBL
+    }
+});
+
+let historyMarkers = history.features.map(d => {
+    return {
+        "lat": d.geometry.coordinates[1],
+        "lng": d.geometry.coordinates[0],
+        "label": d.properties.Name
     }
 });
 
@@ -29,10 +41,11 @@ const map = new GoogleMap({
     data: {
         el: el,
         config: {
-            center: { lat: 21.022154913629862, lng: -71.58270320625003 },
+            center: { lat: 24.266188981138203, lng: -71.71453914375003 },
             zoom: el.clientWidth > 600 ? 5 : 4
         },
         markers: markers,
+        historyMarkers: historyMarkers,
         markerConfig: {
             markerSize: 10,
             infoWindowWidth: 100
@@ -41,17 +54,19 @@ const map = new GoogleMap({
             const colour = getColour(f.getProperty("band"));
             return {
                 fillColor: colour,
-                fillOpacity: 0.35,
+                fillOpacity: 0.55,
                 strokeColor: colour,
                 strokeWeight: 1,
-                strokeOpacity: 0.3
+                strokeOpacity: 0.5
             }
         },
         key: "AIzaSyBGZVyAXHJwoA4Ea-a3kuD1AsuZwbrnLlM"
     }
 })
 
-window.resize();
+if(window.resize) {
+    window.resize();
+}
 
 
 //use this in map config, note f.getProperty(x) rather than f.properties.x
