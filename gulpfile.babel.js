b502077705c8ef1fd035c2b8639dab16ffe786b6
Niko Kommenda <nkommenda@hotmail.com>
2017-09-07 15:42:08 +0100

    deploylive and deploypreview tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -222,6 +222,39 @@ gulp.task('deploy', ['build'], cb => {
     });
 });
 
+gulp.task('deploylive', ['build'], cb => {
+    if(s3Path === "atoms/2016/05/blah") {
+        console.error("ERROR: You need to change the deploy path from its default value")
+        return;
+    }
+
+    gulp.src(`${buildDir}/**/*`)
+        .pipe(s3Upload('max-age=31536000', s3VersionPath))
+        .on('end', () => {
+            gulp.src('config.json')
+                .pipe(file('preview', version))
+                .pipe(file('live', version))
+                .pipe(s3Upload('max-age=30', s3Path))
+                .on('end', cb);
+        });
+});
+
+gulp.task('deploypreview', ['build'], cb => {
+    if(s3Path === "atoms/2016/05/blah") {
+        console.error("ERROR: You need to change the deploy path from its default value")
+        return;
+    }
+    
+    gulp.src(`${buildDir}/**/*`)
+        .pipe(s3Upload('max-age=31536000', s3VersionPath))
+        .on('end', () => {
+            gulp.src('config.json')
+                .pipe(file('preview', version))
+                .pipe(s3Upload('max-age=30', s3Path))
+                .on('end', cb);
+        });
+});
+
 gulp.task('local', ['build'], () => {
     return gulp.src('harness/*')
         .pipe(template({
