8b94c75f1ae94808281ac982ee84f3d8afc596ec
Niko Kommenda <nkommenda@hotmail.com>
2017-09-19 16:37:02 +0100

    labels back on the map

--- src/render.js
+++ src/render.js
@@ -14,10 +14,20 @@ export async function render() {
 
     const date = moment().utcOffset('-0400').format('D MMMM, h:mm a')
 
+    console.log('rendering...')
+
     return `<div class="here">
     	<div class="scale">
+            <div class="scale-component">
+	            <div class="scale__title">Hurricane severity</div>
+	            <div class="scale__section" data-label="5" style="background-color: #850000"></div>
+	            <div class="scale__section" data-label="4" style="background-color: #bd0026"></div>
+	            <div class="scale__section" data-label="3" style="background-color: #f03b20"></div>
+	            <div class="scale__section" data-label="2" style="background-color: #fd8d3c"></div>
+	            <div class="scale__section" data-label="1" style="background-color: #fecc5c"></div>
+    	    </div>
+
     		<div class="scale__section" data-label="The storm's centre is likely to pass through this area" style="background-color: #767676"></div>
-    		<div class="scale__text">Numbers represent the severity of the hurricane</div>
     	</div>
     	<div class="map"></div>
     	<div class="notes">All times in Atlantic Standard Time | Source: US National Hurricane Center | Last updated: ${date}</div>
