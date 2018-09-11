b9ce2a6a2ff9d8a7a0bd432e7ed8b725a0a5bcf7
Josh Holder <josh.holder@theguardian.com>
2018-09-11 10:19:56 +0100

    Comment out iframe code

--- src/js/main.js
+++ src/js/main.js
@@ -3,24 +3,24 @@ var el = document.createElement('script');
 el.src = process.env.PATH + '/app.js';
 document.body.appendChild(el);
 
-function inIframe () {
-    if(window.frameElement) {
-    	return true;
-    } else {
-    	return false;
-    }
-}
+// function inIframe () {
+//     if(window.frameElement) {
+//     	return true;
+//     } else {
+//     	return false;
+//     }
+// }
 
-if(inIframe()) {
-    var html = document.querySelector("html");
-    var body = document.querySelector("body");
+// if(inIframe()) {
+//     var html = document.querySelector("html");
+//     var body = document.querySelector("body");
 
-    html.style.overflow = "hidden";
-    body.style.overflow = "hidden";
+//     html.style.overflow = "hidden";
+//     body.style.overflow = "hidden";
 
-    html.style.padding = 0;
-    body.style.padding = 0;
+//     html.style.padding = 0;
+//     body.style.padding = 0;
 
-    html.style.margin = 0;
-    body.style.margin = 0;
-}
+//     html.style.margin = 0;
+//     body.style.margin = 0;
+// }
